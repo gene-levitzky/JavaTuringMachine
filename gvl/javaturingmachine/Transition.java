9f4031cf8b328451bc6cd10570f5680caaa3a8f8
Gene <gene.levitzky@gmail.com>
2015-01-20 18:22:35 -0500

    Added bare methods to Transition.

--- gvl/javaturingmachine/Transition.java
+++ gvl/javaturingmachine/Transition.java
@@ -2,4 +2,15 @@ package gvl.javaturingmachine;
 
 public class Transition {
 
+	public Symbol getNewSymbol() {
+		return null;
+	}
+	
+	public State getNewState() {
+		return null;
+	}
+	
+	public Direction getNewDirection() {
+		return Direction.STAY;
+	}
 }
