03bf399095d1305304a844fc2e43b135f57065d5
Gene <gene.levitzky@gmail.com>
2015-01-20 18:25:53 -0500

    Added class descriptionfor Transition.

--- gvl/javaturingmachine/Transition.java
+++ gvl/javaturingmachine/Transition.java
@@ -1,5 +1,12 @@
 package gvl.javaturingmachine;
 
+/**
+ * This class represents an atomic transition for the TuringMachine. The transition indicates the symbol to write to 
+ * the tape, the new state to enter, and the new direction.
+ * 
+ * @author Gene
+ *
+ */
 public class Transition {
 	
 	private Symbol newSymbol;
