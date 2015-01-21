50af0c0bddad0541066091dedc6870a564c22e8c
Gene <gene.levitzky@gmail.com>
2015-01-20 20:20:15 -0500

    Modified Tape.java, Symbol.java.

--- gvl/javaturingmachine/Symbol.java
+++ gvl/javaturingmachine/Symbol.java
@@ -10,12 +10,18 @@ public class Symbol {
 	
 	private String stringRepresentation;
 	
+	public static final Symbol BLANK = new Symbol(" ");
+	
 	public Symbol (String stringRepresentation) {
 		this.stringRepresentation = stringRepresentation;
 	}
 	
 	public String getStringRepresentation() {
 		return stringRepresentation;
+	}
+	
+	public String toString() {
+		return this.getStringRepresentation();
 	}
 
 }
--- gvl/javaturingmachine/Tape.java
+++ gvl/javaturingmachine/Tape.java
@@ -1,6 +1,20 @@
 package gvl.javaturingmachine;
 
+import java.util.List;
+
 public class Tape {
 
+	private List<Symbol> tape;
 	
+	public Tape(List<Symbol> tape) {
+		this.tape = tape;
+	}
+	
+	public Symbol get(int index) {
+		if (index >= tape.size()) {
+			tape.add(Symbol.BLANK);
+		}
+		
+		return tape.get(index);
+	}
 }
